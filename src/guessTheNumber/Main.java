1c5ac78a4c8746da3bf920a7d7b8ba2eba08d0af
drew <82567656+drew-byte@users.noreply.github.com>
2022-12-07 13:25:42 +0800

    guess

--- src/guessTheNumber/Main.java
+++ src/guessTheNumber/Main.java
@@ -63,40 +63,40 @@ public class Main {
 		
 		
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBackground(new Color(255, 255, 255));
-		panel_1.setBounds(394, 149, 512, 75);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel panel1 = new JPanel();
+		panel1.setBackground(new Color(255, 255, 255));
+		panel1.setBounds(394, 149, 512, 75);
+		panel.add(panel1);
+		panel1.setLayout(null);
 		
-		JLabel lblGuessTheNumber = new JLabel("Guess the number [ 1 - 10 ]");
-		lblGuessTheNumber.setBounds(0, 19, 512, 45);
-		panel_1.add(lblGuessTheNumber);
-		lblGuessTheNumber.setFont(new Font("Bauhaus 93", Font.BOLD, 40));
-		lblGuessTheNumber.setHorizontalAlignment(SwingConstants.CENTER);
+		JLabel header = new JLabel("Guess the number [ 1 - 10 ]");
+		header.setBounds(0, 19, 512, 45);
+		panel1.add(header);
+		header.setFont(new Font("Bauhaus 93", Font.BOLD, 40));
+		header.setHorizontalAlignment(SwingConstants.CENTER);
 		
-		JPanel panel_2 = new JPanel();
-		panel_2.setBackground(new Color(192, 192, 192));
-		panel_2.setBounds(416, 235, 459, 304);
-		panel.add(panel_2);
-		panel_2.setLayout(null);
+		JPanel panel2 = new JPanel();
+		panel2.setBackground(new Color(192, 192, 192));
+		panel2.setBounds(416, 235, 459, 304);
+		panel.add(panel2);
+		panel2.setLayout(null);
 		
 		textField = new JTextField();
 		textField.setBackground(Color.WHITE);
 		textField.setBounds(94, 173, 171, 40);
-		panel_2.add(textField);
+		panel2.add(textField);
 		textField.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Input here below :");
-		lblNewLabel.setFont(new Font("Bahnschrift", Font.BOLD, 20));
-		lblNewLabel.setBounds(94, 136, 282, 53);
-		panel_2.add(lblNewLabel);
+		JLabel label1 = new JLabel("Input here below :");
+		label1.setFont(new Font("Bahnschrift", Font.BOLD, 20));
+		label1.setBounds(94, 136, 282, 53);
+		panel2.add(label1);
 		
 		    Random rand = new Random();    
 		    int randomNum = rand.nextInt(10);
 		
-		JButton btnNewButton = new JButton("Submit");
-		btnNewButton.addActionListener(new ActionListener() {
+		JButton btn = new JButton("Submit");
+		btn.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				
 			  
@@ -120,37 +120,36 @@ public class Main {
 					}
 					else if(num==randomNum)
 					{
-						JOptionPane.showMessageDialog(null, "Congrats! the correct number is " + num);
+						JOptionPane.showMessageDialog(null, "Congrats! The correct number is " + num +" !");
 					}
 				}
+				textField.setText("");
 			}
+			
 		});
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		btnNewButton.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 14));
-		btnNewButton.setBounds(275, 172, 100, 40);
-		panel_2.add(btnNewButton);
+		btn.setBackground(new Color(255, 255, 255));
+		btn.setFont(new Font("Tw Cen MT Condensed Extra Bold", Font.PLAIN, 14));
+		btn.setBounds(275, 172, 100, 40);
+		panel2.add(btn);
 		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(255, 255, 255));
-		panel_3.setBounds(94, 40, 282, 69);
-		panel_2.add(panel_3);
-		panel_3.setLayout(null);
+		JPanel panel3 = new JPanel();
+		panel3.setBackground(new Color(255, 255, 255));
+		panel3.setBounds(94, 40, 282, 69);
+		panel2.add(panel3);
+		panel3.setLayout(null);
 		
 		JLabel lblNewLabel_2 = new JLabel("Number");
 		lblNewLabel_2.setBounds(57, 21, 165, 37);
-		panel_3.add(lblNewLabel_2);
+		panel3.add(lblNewLabel_2);
 		lblNewLabel_2.setFont(new Font("Bauhaus 93", Font.BOLD, 30));
 		
 		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setBounds(172, 0, 50, 66);
-		panel_3.add(lblNewLabel_1);
+		panel3.add(lblNewLabel_1);
 		lblNewLabel_1.setIcon(new ImageIcon(new ImageIcon("C:\\Users\\Shane\\Desktop\\Icons for HCI\\question-sign.png")
 				.getImage().getScaledInstance(50, 50, Image.SCALE_DEFAULT)));
 		
 		JLabel label = new JLabel("");
-		label.setHorizontalAlignment(SwingConstants.CENTER);
-		label.setLabelFor(lblNewLabel);
-		label.setFont(new Font("Calibri", Font.BOLD, 20));
 		label.setIcon(new ImageIcon("C:\\Users\\Shane\\Desktop\\Icons for HCI\\dreamy-lines-8k-go-1280x720.jpg"));
 		label.setBounds(0, 0, 1264, 681);
 		panel.add(label);
